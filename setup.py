ba7dbc0b62c8ab595ce08b0dea618debd28916c8
chipo <dev@chipo.icu>
2019-12-22 13:33:19 +0900

    add v1.0

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup, Extension
+
+skein_hash_module = Extension('skein_hash',
+                               sources = ['skeinmodule.c',
+                                          'skein.c'],
+                               include_dirs=['.', './shacommon'])
+
+setup (name = 'skein_hash',
+       version = '1.0',
+       package_data = {
+        '': ['*.h']
+        },
+       license="MIT",
+       author = 'nakkie',
+       author_email = 'nakkie@linux.jpn.com',
+       maintainer='nakkie',
+       maintainer_email='nakkie@linux.jpn.com',
+       description = 'Binding for CHIPO skein proof of work hashing.',
+       ext_modules = [skein_hash_module],
+       url = 'https://github.com/CHIPO-Project/skein-hash-python',
+       download_url = 'https://github.com/CHIPO-Project/skein-hash-python/archive/v1.0.tar.gz'
+       )
